a9e6ac8f85f202d4e73030a4af57d79d18bd81bc
Alican Çubukçuoğlu <alicancubukcuoglu@gmail.com>
2022-08-06 22:40:09 +0300

    asd

--- test/src/main.rs
+++ test/src/main.rs
@@ -1,39 +1,25 @@
 mod e2e_vm_tests;
 mod ir_generation;
 
-fn run() {
-    let mut locked = false;
-    let mut filter_regex = None;
-    for arg in std::env::args().skip(1) {
-        // Check for the `--locked` flag. Must precede the regex.
-        // Intended for use in `CI` to ensure test lock files are up to date.
-        if arg == "--locked" {
-            locked = true;
-            continue;
-        }
+use clap::Parser;
 
-        // Check for a regex, used to filter the set of tests.
-        let regex = regex::Regex::new(&arg).unwrap_or_else(|_| {
-            panic!(
-                "Expected either `--locked` or a filter regex, found: {:?}.",
-                arg
-            )
-        });
-        filter_regex = Some(regex);
-    }
+#[derive(Parser)]
+struct Cli {
+    /// If specified, only run tests matching this regexp
+    #[clap(value_parser)]
+    filter_regex: Option<regex::Regex>,
 
-    e2e_vm_tests::run(locked, filter_regex.as_ref());
-    ir_generation::run(filter_regex.as_ref());
+    /// Intended for use in `CI` to ensure test lock files are up to date
+    #[clap(long)]
+    locked: bool,
+
+    #[clap(long, short)]
+    quiet: bool,
 }
 
 fn main() {
-    run()
-}
+    let cli = Cli::parse();
 
-#[test]
-fn test() {
-    run()
+    e2e_vm_tests::run(cli.locked, cli.filter_regex.as_ref());
+    ir_generation::run(cli.filter_regex.as_ref());
 }
-// #[cfg(test)]
-// mod tests {
-// }
